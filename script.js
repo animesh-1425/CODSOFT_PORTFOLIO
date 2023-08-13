6bfec36d454a5db2af430908d9622cd33290527f
Animesh Dey <animesh1425@gmail.com>
2023-08-13 22:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const toggle=document.querySelector(".toggle");
 toggle.addEventListener("click",()=>{
   menu.classList.toggle("active");
 })
+var icon=document.getElementById("icon");
+
+icon.onclick=function(){
+  document.body.classList.toggle("dark");
+  if(document.body.classList.contains("dark")){
+    icon.src="sun.png";
+  }
+  else{
+    icon.src="moon.png";
+  }
+}
